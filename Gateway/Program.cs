cba4ce8488ab4be8bfeedbc6b269e1ac76fd91a4
Ilya Grigoryan <ilya.grigoryan@digitain.com>
2023-08-02 19:01:36 +0400

    configure ocelot to just run

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,4 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Load and add Ocelot configuration from ocelot.json
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json")
+    .Build();
 
 // Add services to the container.
 
@@ -7,7 +16,17 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddOcelot(configuration);
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +40,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseOcelot().Wait();
 
 app.Run();
